785ea9309fdf679856aad8bc86ef988a3b909fb4
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-04-11 16:15:48 +0400

    Update favicon size

--- src/components/seo.js
+++ src/components/seo.js
@@ -34,7 +34,7 @@ const SEO = ({ description, lang, meta, title }) => {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`${site.siteMetadata.title}`}
       meta={[
         {
           name: 'description',
--- src/html.js
+++ src/html.js
@@ -19,11 +19,11 @@ export default function HTML({
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {headComponents}
         <link
           href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           rel="stylesheet"
         />
-        {headComponents}
       </head>
       <body {...bodyAttributes}>
         {preBodyComponents}
