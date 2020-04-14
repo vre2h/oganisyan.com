87ce52121add48368acf828ea8d5a303bf6406dc
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-04-14 23:05:11 +0400

    Remove duplications

--- src/html.js
+++ src/html.js
@@ -18,7 +18,6 @@ export default function HTML({
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
