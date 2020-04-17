54e23034818986fb248e4553ef751caf1852abba
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-04-17 18:41:16 +0400

    Update components

--- src/pages/bookshelf.js
+++ src/pages/bookshelf.js
@@ -8,32 +8,37 @@ import Flex from '../components/Flex'
 import { rhythm } from '../utils/typography'
 import { Bookmark } from '../components/icons'
 
-const Item = ({ title, author, review }) => (
+const Book = ({ title, author, review }) => (
   <Flex flexDirection="row" style={{ marginBottom: rhythm(1) }}>
-    <div>
-      <header style={{}}>
-        <div style={{ display: 'flex' }}>
-          <Bookmark
-            width="25"
-            style={{
-              marginRight: '10px',
-              alignSelf: 'flex-start',
-              cursor: 'pointer',
-              fill: 'var(--main-color)',
-            }}
-          />
-          <div>
-            <h4 style={{ margin: 0 }}>
-              {title} {review}
-            </h4>
-            <small style={{ fontStyle: 'italic', color: '#a5a5a5' }}>
-              by {author}
-            </small>
-          </div>
-        </div>
-      </header>
+    <div style={{ display: 'flex' }}>
+      <Bookmark
+        width="25"
+        style={{
+          marginRight: '10px',
+          alignSelf: 'flex-start',
+          cursor: 'pointer',
+          fill: 'var(--main-color)',
+        }}
+      />
+      <div>
+        <h4 style={{ margin: 0 }}>
+          {title} {review}
+        </h4>
+        <small style={{ fontStyle: 'italic', color: '#a5a5a5' }}>
+          by {author}
+        </small>
+      </div>
     </div>
   </Flex>
+)
+
+const BookList = ({ books, title }) => (
+  <section>
+    <h4>{title}</h4>
+    {books.map(({ title: bookTitle, author, review }) => (
+      <Book title={bookTitle} author={author} review={review} />
+    ))}
+  </section>
 )
 
 export default ({ data, location }) => {
@@ -47,34 +52,42 @@ export default ({ data, location }) => {
       <h1 style={{ margin: `${rhythm(1 / 2)} 0 ${rhythm(2)} 0` }}>Bookshelf</h1>
 
       <main style={{ marginTop: rhythm(1) }}>
-        <div>
-          <h4>2020</h4>
-          <Item title="War and Peace" author="Leo Tolstoy" review="🔥🔥🔥" />
-          <Item
-            title="Steve Jobs"
-            author="Walter Isaacson"
-            review="🔥🔥🔥🔥🔥"
-          />
-          <Item title="The Jedi Way" author="Maxim Dorofeev" review="🔥" />
-        </div>
-        <div>
-          <h4>2019</h4>
-          <Item title="War and Peace" author="Leo Tolstoy" />
-          <Item title="Steve Jobs" author="Walter Isaacson" />
-          <Item title="The Jedi Way" author="Maxim Dorofeev" />
-          <Item title="War and Peace" author="Leo Tolstoy" />
-          <Item title="Steve Jobs" author="Walter Isaacson" />
-          <Item title="The Jedi Way" author="Maxim Dorofeev" />
-          <Item title="War and Peace" author="Leo Tolstoy" />
-          <Item title="Steve Jobs" author="Walter Isaacson" />
-          <Item title="The Jedi Way" author="Maxim Dorofeev" />
-        </div>
-        <div>
-          <h4>2018</h4>
-          <Item title="War and Peace" author="Leo Tolstoy" />
-          <Item title="Steve Jobs" author="Walter Isaacson" />
-          <Item title="The Jedi Way" author="Maxim Dorofeev" />
-        </div>
+        <BookList
+          books={[
+            { title: 'War and Peace', author: 'Leo Tolstoy', review: '🔥🔥🔥' },
+            {
+              title: 'Steve Jobs',
+              author: 'Walter Isaacson',
+              review: '🔥🔥🔥🔥🔥',
+            },
+            { title: 'The Jedi Way', author: 'Maxim Dorofeev', review: '🔥' },
+          ]}
+          title="2020"
+        />
+        <BookList
+          books={[
+            { title: 'War and Peace', author: 'Leo Tolstoy', review: '🔥🔥🔥' },
+            {
+              title: 'Steve Jobs',
+              author: 'Walter Isaacson',
+              review: '🔥🔥🔥🔥🔥',
+            },
+            { title: 'The Jedi Way', author: 'Maxim Dorofeev', review: '🔥' },
+          ]}
+          title="2019"
+        />
+        <BookList
+          books={[
+            { title: 'War and Peace', author: 'Leo Tolstoy', review: '🔥🔥🔥' },
+            {
+              title: 'Steve Jobs',
+              author: 'Walter Isaacson',
+              review: '🔥🔥🔥🔥🔥',
+            },
+            { title: 'The Jedi Way', author: 'Maxim Dorofeev', review: '🔥' },
+          ]}
+          title="2018"
+        />
       </main>
     </Layout>
   )
