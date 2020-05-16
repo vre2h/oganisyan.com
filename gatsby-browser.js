d4b70ebed03f6a4737534e410c07a85ecf63b077
Vrezh Oganisyan <hovanisyan.vrezh@gmail.com>
2020-05-16 16:41:36 +0400

    add smoothscroll support for all browsers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
+import smoothscroll from 'smoothscroll-polyfill'
+
 // custom typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
 import './src/styles/index.css'
+
+smoothscroll.polyfill()
